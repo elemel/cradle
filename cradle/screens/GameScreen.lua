dcb0dded0261f140db3f3b28e690b39b80a71027
Mikael Lind <elemel@elemel.se>
2023-06-18 22:04:01 +0200

    Add fixed time step

--- cradle/screens/GameScreen.lua
+++ cradle/screens/GameScreen.lua
@@ -1,5 +1,6 @@
 local Class = require("cradle.Class")
 local heart = require("heart")
+local UpdateClockHandler = require("cradle.handlers.UpdateClockHandler")
 
 local M = Class.new()
 
@@ -7,12 +8,24 @@ function M:init(application)
   self.application = assert(application)
   self.engine = heart.newEngine()
 
+  self.engine:setProperty("clock", {
+    fixedDt = 1 / 60,
+    accumulatedDt = 0,
+    maxAccumulatedDt = 0.1,
+  })
+
   self.engine:addEvent("draw")
+  self.engine:addEvent("fixedupdate")
   self.engine:addEvent("keypressed")
   self.engine:addEvent("keyreleased")
   self.engine:addEvent("mousemoved")
   self.engine:addEvent("resize")
   self.engine:addEvent("update")
+
+  local world = love.physics.newWorld(0, 10)
+  self.engine:setProperty("world", world)
+
+  self.engine:addEventHandler("update", UpdateClockHandler.new(self.engine))
 end
 
 function M:handleEvent(event, ...)
